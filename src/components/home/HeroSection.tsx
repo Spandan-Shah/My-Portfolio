c9628d9fe813b509254fee2afc39482cba9d80e9
Spandan Shah <spandanshah1503@gmail.com>
2026-04-29 12:04:19 +0530

    style: Add Animated Gradient Name Text

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -36,9 +36,12 @@ const HeroSection = () => (
           <p className="font-mono-cyber text-md tracking-widest text-white animate-flicker">
             // SYSTEM.ONLINE — SECURE CONNECTION ESTABLISHED
           </p>
-          <h1 className="font-mono-cyber text-4xl font-bold leading-tight tracking-tight text-foreground md:text-5xl lg:text-6xl">
-            Hi, I'm <span className="text-primary text-red">Spandan Shah</span>
-          </h1>
+        <h1 className="font-mono-cyber text-4xl font-bold leading-tight tracking-tight text-foreground md:text-5xl lg:text-6xl">
+          Hi, I'm{" "}
+          <span className="animate-gradient bg-gradient-to-r from-purple-600 via-red-500 to-green-600 bg-[length:200%_200%] bg-clip-text text-transparent">
+            Spandan Shah
+          </span>
+        </h1>
           <h2 className="font-display text-lg tracking-wider text-yellow-500 md:text-xl">
             <TypeAnimation
               sequence={[
