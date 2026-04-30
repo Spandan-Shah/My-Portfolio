99965ac975cc23e565801d913220ce230cce58d4
Spandan Shah <spandanshah1503@gmail.com>
2026-04-30 12:23:11 +0530

    feat: Add GSSP Image To Tracing Beam

--- src/components/tracing-beam-demo.tsx
+++ src/components/tracing-beam-demo.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import GSSP from "@/assets/GSSP.png";
 
 
 import { twMerge } from "tailwind-merge";
@@ -67,8 +68,7 @@ const dummyContent = [
       </>
     ),
     badge: "React",
-    image:
-      "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image:GSSP,
   },
   {
     title: "Lorem Ipsum Dolor Sit Amet",
