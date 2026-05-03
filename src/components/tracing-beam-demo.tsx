172ae7a24fe721f081f3eb30f58d51e99f63a5d0
Spandan Shah <spandanshah1503@gmail.com>
2026-05-04 00:18:45 +0530

    feat: Add Berlin Computer Vision Project Section

--- src/components/tracing-beam-demo.tsx
+++ src/components/tracing-beam-demo.tsx
@@ -36,8 +36,8 @@ export default function TracingBeamDemo() {
                 <img
                   src={item.image}
                   alt="blog thumbnail"
-                  height="1000"
-                  width="1000"
+                  height="1792"
+                  width="1024"
                   className="rounded-lg mb-10 object-cover"
                 />
               )}
@@ -181,7 +181,7 @@ badges: [
       "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "Lorem Ipsum Dolor Sit Amet",
+    title: "Berlin - An Intelligent Computer Vision Framework for Vehicle Detection, Tracking, and Traffic Analysis Using CCTV Video Streams",
     description: (
       <>
         <p>
